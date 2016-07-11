36920df4512cdc52ffd8907029964e39cf63b404
Peter Wagener <peterwagener@gmx.de>
2016-07-11 12:07:54 +0200

    fixed address translator test bench

--- tb_address_translator.vhd
+++ tb_address_translator.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_arith.all;
 
 library paco_lut;
 use paco_lut.lut_package.all;
+use paco_lut.test_package.all;
 
 entity tb_address_translator is
 end entity;
@@ -13,29 +14,21 @@ architecture behavior of tb_address_translator is
   
   signal clk : std_logic;
 
-  type i_signals_t is record
-    id_rst_i : std_logic;
-    cfg_i : cfg_word_t;
-    pipeline_i : p_pla_t;
-  end record;
-
-  type o_signals_t is record
-    cfg_o : cfg_word_t;
-    pipeline_o : p_lut_t;
-  end record;
-
-  signal i_signals: i_signals_t;
-  signal o_signals: o_signals_t;
+  signal i_signals: pla_i_signals_t;
+  signal o_signals: pla_o_signals_t;
    
-  procedure init_signals(signal i: out i_signals_t) is 
+  procedure init_signals(signal i: out pla_i_signals_t) is 
   begin
     i.id_rst_i <= '0';
     i.cfg_i.valid <= '0';
+    i.cfg_i.d <= (others => '0');
     i.pipeline_i.valid <= '0';
+    i.pipeline_i.interpolator <= (others => '0');
+    i.pipeline_i.selector <= (others => '0');
   end procedure;
   
   procedure test_configuration(
-    signal i: out i_signals_t; signal o: o_signals_t) is
+    signal i: out pla_i_signals_t; signal o: pla_o_signals_t) is
   begin
     assert
       o.cfg_o.valid='0'
@@ -86,7 +79,66 @@ architecture behavior of tb_address_translator is
     end loop;
     i.cfg_i.valid <= '0';
   end procedure;
+  
+  procedure reset(
+    signal i: out pla_i_signals_t; signal o: pla_o_signals_t) is
+  begin
+    i.id_rst_i <= '1';
+    wait for C_CLK_PERIOD;
+    i.id_rst_i <= '0';
+  end procedure;
 
+  procedure configure_and_incremental(
+    signal i: out pla_i_signals_t; signal o: pla_o_signals_t) is
+  begin
+    i.cfg_i.valid <= '1';
+    -- configure AND plane
+    for j in 0 to C_PLA_INTERCONNECTS-1 loop
+      i.cfg_i.d <= conv_std_logic_vector(j,C_CFG_WORD_SIZE);
+      wait for C_CLK_PERIOD;
+      if C_CFG_PLA_AND_REGISTERS_PER_ROW>1 then
+        i.cfg_i.d <= (others => '0');
+        wait for C_CLK_PERIOD*(C_CFG_PLA_AND_REGISTERS_PER_ROW-1);
+      end if;
+    end loop;
+    i.cfg_i.valid <= '0';
+  end procedure;
+  procedure configure_or_single(
+    signal i: out pla_i_signals_t; signal o: pla_o_signals_t;
+    v: std_logic_vector) is
+  begin
+    i.cfg_i.valid <= '1';
+    -- configure OR plane
+    i.cfg_i.d <= v;
+    wait for C_CLK_PERIOD;
+    i.cfg_i.d <= (others => '0');
+    if C_SEGMENT_BITS>1 then
+      wait for C_CLK_PERIOD*(C_SEGMENT_BITS-1);
+    end if;
+    i.cfg_i.valid <= '0';
+  end procedure;
+
+
+  procedure test_pla_1(
+    signal i: out pla_i_signals_t; signal o: pla_o_signals_t) is
+  begin
+    reset(i,o);
+
+    for j in 0 to 2**C_PLA_INTERCONNECTS-1 loop
+      configure_or_single(i,o,conv_std_logic_vector(j,C_CFG_WORD_SIZE));
+      configure_and_incremental(i,o);
+      
+      i.pipeline_i.valid <= '1';
+      for k in 0 to 2**C_SELECTOR_BITS-1 loop
+        i.pipeline_i.selector <= conv_std_logic_vector(k,C_SELECTOR_BITS);
+        wait for C_CLK_PERIOD;
+      end loop;
+      i.pipeline_i.valid <= '0';
+    end loop;
+    
+
+  end procedure;
+  
 begin
   
   uut: address_translator port map (
@@ -116,6 +168,11 @@ begin
     wait for C_CLK_PERIOD/2;
 
     test_configuration(i_signals,o_signals);
+    -- this performs some rudimentary test case to be evaluated MANUALLY.
+    -- for more thorough testing, synthesize using ht_address_translator as
+    -- top-level module and use `ht_address_translator.py -r` to run automated
+    -- testing.
+    test_pla_1(i_signals,o_signals);
 
     wait;
   end process;
